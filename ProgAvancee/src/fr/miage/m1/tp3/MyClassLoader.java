5514fe4b287795e16f022b2ed2006eaf341c2f1c
Gael Rottier <gaelrottier@gmail.com>
2014-10-04 01:02:05 +0200

    MyClassLoader

--- ProgAvancee/src/fr/miage/m1/tp3/MyClassLoader.java
+++ ProgAvancee/src/fr/miage/m1/tp3/MyClassLoader.java
@@ -15,6 +15,7 @@ import java.util.logging.Logger;
 public class MyClassLoader extends SecureClassLoader {
 
     private ArrayList<File> path = new ArrayList<File>();
+    private File result = null;
 
     public void setPath(String... paths) {
         if (paths.length > 0) {
@@ -31,53 +32,75 @@ public class MyClassLoader extends SecureClassLoader {
     }
 
     private byte[] loadClassData(String name) {
-        FileInputStream fis = null;
         try {
+            FileInputStream fis = null;
             String[] className = name.split("\\.");
-            File result = null;
-            do {
-                for (File p : path) {
-                    result = checkFile(p, className, 0);
-                }
-            } while (result == null);
-
+            for (File p : path) {
+                findFile(p, className, 0);
+            }
             fis = new FileInputStream(result);
             ByteArrayOutputStream bis = new ByteArrayOutputStream(fis.read());
-
             return bis.toByteArray();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        } finally {
-            try {
-                fis.close();
-            } catch (IOException ex) {
-                Logger.getLogger(MyClassLoader.class.getName()).log(Level.SEVERE, null, ex);
-            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(MyClassLoader.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(MyClassLoader.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
 
     }
 
-    private File checkFile(File dir, String[] packageName, int iteration) {
+    private void findFile(File dir, String[] packageName, int iteration) {
 
-        System.out.println(dir);
-        if (dir.isDirectory()) {
-            for (File f : dir.listFiles()) {
-                if (f.isDirectory()) {
-                    if (f.getName().equals(packageName[iteration])) {
-                        checkFile(f, packageName, iteration + 1);
-                    } else {
-                        checkFile(f, packageName, 0);
+        if (result == null) {
+
+//            System.out.println("\nL'arborescence de fichiers actuelle est : " + dir.getAbsolutePath());
+//            System.out.println("Le fichier recherché est : " + packageName[iteration]);
+//            System.out.println("Le fichier courant est : " + dir.getName());
+            if (dir.isDirectory()) {
+
+//                System.out.println("  et est un dossier");
+                for (File f : dir.listFiles()) {
+                    if (result == null) {
+
+//                        System.out.println("\tLe fichier " + f.getName() + ", est présent dans le dossier");
+                        if (f.isDirectory()) {
+
+//                            System.out.println("\t  est un dossier");
+                            if (f.getName().equals(packageName[iteration])) {
+
+//                                System.out.println("\t\tLe nom du dossier correspond au "
+//                                        + "dossier recherché : " + packageName[iteration]
+//                                        + ", on incrémente itération à : " + (iteration + 1));
+                                findFile(f, packageName, iteration + 1);
+                            } else {
+
+//                                System.out.println("\t\tLe nom du dossier ("
+//                                        + f.getName() + ") ne correspond pas à celui "
+//                                        + "recherché, on remet itération à 0");
+                                findFile(f, packageName, 0);
+                            }
+                        } else {
+
+//                            System.out.println("\t  est un fichier");
+                            if (f.getName().equals(packageName[packageName.length - 1] + ".class")) {
+
+//                                System.out.println("\t\tLe nom du fichier (" + f.getName()
+//                                        + ") correspond au fichier recherché");
+//                                System.out.println("\t\t\tFichier : " + f);
+                                result = f;
+                            } else {
+
+//                                System.out.println("\t\tLe nom du fichier (" + f.getName()
+//                                        + ") ne correspond pas au fichier recherché ("
+//                                        + packageName[packageName.length - 1] + ".class)");
+//                                
+                            }
+                        }
                     }
                 }
             }
-        } else if (iteration == packageName.length - 1) {
-            return dir;
-
         }
-
-        return null;
-
     }
 
     public static void main(String[] args) throws ClassNotFoundException {
